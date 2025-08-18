73b5ac578e1df11c5f21a359089df8d9e905bfbb
mathiasveraucsm <131810162+mathiasveraucsm@users.noreply.github.com>
2025-08-17 23:35:21 -0500

    Update Ejercicio2.java

--- Practica 1/Ejercicio 2/Ejercicio2.java
+++ Practica 1/Ejercicio 2/Ejercicio2.java
@@ -1,1 +1,34 @@
+package Ejercicios;
+import java.util.Scanner;
 
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);		
+		int[] numeros = new int[10];
+		
+		System.out.println("Ingrese el primer nro: ");
+		numeros[0] = scanner.nextInt(); //como el cin
+		
+		for(int i = 1; i < 10; i++) {
+			int num;
+			while(true) {
+				System.out.println("Ingrese el número "+ (i+1)+": "); //como el cout
+				num = scanner.nextInt();//cin
+				
+				if(num > numeros[i-1]) {
+					numeros[i] = num; //agregando
+					break;
+				} else {
+					System.out.println("El número debe ser mayor que " + numeros[i-1]+". Intente nuevamente.");
+				}
+			}
+		}
+		System.out.println("\nNúmeros ingresados: ");
+		for(int n: numeros) {
+			System.out.println(n);
+		}
+
+	}
+
+}
