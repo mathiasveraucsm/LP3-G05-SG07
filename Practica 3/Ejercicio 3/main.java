7474528c565b781d0f9695aa8476ce001d4a544b
mathiasveraucsm <131810162+mathiasveraucsm@users.noreply.github.com>
2025-09-07 23:38:41 -0500

    Create main.java

---
+++ Practica 3/Ejercicio 3/main.java
@@ -0,0 +1,16 @@
+package ejercicio3;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Vehiculo v1 = new Coche();
+        Vehiculo v2 = new Bicicleta();
+
+        v1.acelerar(); 
+        v2.acelerar(); 
+
+
+	}
+
+}
