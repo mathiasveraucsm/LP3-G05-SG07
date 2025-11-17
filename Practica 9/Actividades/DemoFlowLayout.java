b76fc60c97645e74ec8fa8ae87c97f932e82bdd1
mathiasveraucsm <131810162+mathiasveraucsm@users.noreply.github.com>
2025-11-16 23:24:41 -0500

    Add files via upload

---
+++ Practica 9/Actividades/DemoFlowLayout.java
@@ -0,0 +1,16 @@
+package Actividades;
+
+ // Fig. 12.40: DemoFlowLayout.java
+ // Prueba MarcoFlowLayout.
+ import javax.swing.JFrame;
+
+ public class DemoFlowLayout
+ {
+ public static void main(String[] args)
+ {
+ MarcoFlowLayout marcoFlowLayout = new MarcoFlowLayout();
+ marcoFlowLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ marcoFlowLayout.setSize(300, 75);
+ marcoFlowLayout.setVisible(true);
+ }
+ } // fin de la clase DemoFlowLayout
---
+++ Practica 9/Actividades/DemoGridLayout.java
@@ -0,0 +1,15 @@
+package Actividades;
+ // Fig. 12.44: DemoGridLayout.java
+ // Prueba de MarcoGridLayout.
+ import javax.swing.JFrame;
+
+ public class DemoGridLayout
+ {
+ public static void main(String[] args)
+ {
+ MarcoGridLayout marcoGridLayout = new MarcoGridLayout();
+ marcoGridLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ marcoGridLayout.setSize(300, 200);
+ marcoGridLayout.setVisible(true);
+ }
+ } // fin de la clase DemoGridLayout
