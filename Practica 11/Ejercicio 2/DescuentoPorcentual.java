49234cf988496c5a2091af1e4b9c4c3d0557bdbf
mathiasveraucsm <131810162+mathiasveraucsm@users.noreply.github.com>
2025-12-02 23:10:47 -0500

    Update DescuentoPorcentual.java

--- Practica 11/Ejercicio 2/DescuentoPorcentual.java
+++ Practica 11/Ejercicio 2/DescuentoPorcentual.java
@@ -7,7 +7,7 @@ public class DescuentoPorcentual implements Descuento {
 	public double calcularPrecioFinal(Producto[] productos) {
 		
 		if (productos.length == 2 && productos[0].getNombre().equals(productos[1].getNombre())) {
-			return (productos[0].getPrecio() + productos[1].getPrecio()) * 0.7; // 30% descuento
+			return (productos[0].getPrecio() + productos[1].getPrecio()) * 0.7; 
 		}
 		
 		double total = 0;
@@ -15,4 +15,5 @@ public class DescuentoPorcentual implements Descuento {
 		return total;
 		
 	}
+
 }
